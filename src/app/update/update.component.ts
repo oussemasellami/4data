10bf832b239fa0cfc0275236b8e6015af0f7a16d
oussema.sellami <49963438+oussemasellami@users.noreply.github.com>
2025-05-09 16:42:22 +0100

    end!!

--- src/app/update/update.component.ts
+++ src/app/update/update.component.ts
@@ -1,7 +1,8 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 import { ResidenceService } from '../service/residence.service';
 import { Residence } from 'src/core/models/residence';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-update',
@@ -9,15 +10,36 @@ import { Residence } from 'src/core/models/residence';
   styleUrls: ['./update.component.css']
 })
 export class UpdateComponent implements OnInit {
-
-  constructor(private act:ActivatedRoute,private resService:ResidenceService){}
+ addform!:FormGroup 
+  constructor(private act:ActivatedRoute,private route:Router,private resService:ResidenceService){}
   id!:any
   list=new Residence()
     ngOnInit(): void {
       this.id=this.act.snapshot.params['ide']
+
+        this.addform=new FormGroup({
+          //id:new FormControl('',[Validators.required,Validators.minLength(5)]),
+          name:new FormControl('',[Validators.required,Validators.pattern(/^[A-Z][a-z]/)]),
+          address:new FormControl('',Validators.required),
+          image:new FormControl('',Validators.required),
+          status:new FormControl('',[Validators.required,Validators.pattern(/^disponible$/)])
+        })
       this.resService.getResidence(this.id).subscribe((data)=>{
   this.list=data
+  this.addform.patchValue(this.list as any)
       })
+
+}
+
+get status(){
+  return this.addform.get('status')
+}
+update(){
+
+  this.resService.updateResidence(this.id,this.addform.value).subscribe(()=>{
+    console.log("updated!!!!")
+    this.route.navigate(['/residence'])
+  })
 
 }
 }
